20198e226c4d74255e12adddf620741f1970a8a4
Mike Buonomo <mrb2590@gmail.com>
2023-06-30 00:06:35 -0400

    2-install-laravel-breeze-with-inertia-vue (#15)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,7 +35,9 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\HandleInertiaRequests::class,
+            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class
         ],
 
         'api' => [
