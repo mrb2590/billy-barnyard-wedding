c649c36d7e92b13b9e18a23c861fad5b48a8e3a1
Mike Buonomo <mrb2590@gmail.com>
2023-07-12 12:46:37 -0400

    58-fix-ournames-animation (#59)

--- resources/js/Library/animations.js
+++ resources/js/Library/animations.js
@@ -1,52 +1,60 @@
-import {onUnmounted} from 'vue';
+export class MousePerspectiveAnimation {
+  constructor(gsap, elRef, containerId = null) {
+    this.gsap = gsap;
+    this.elRef = elRef;
+    this.mouseX = 0;
+    this.mouseY = 0;
+    this.screenCenter = {x: 0, y: 0};
+    this.frameRequest = null;
+    this.container = containerId ? document.getElementById(containerId) : window;
+    this.setScreenCenter();
+  }
 
-export const animateMousePerspective = (gsap, elRef) => {
-  return {
-    mouse: {x: 0, y: 0},
-    screenCenter: {x: 0, y: 0},
-    frameRequest: null,
-    mouseMove: function (event) {
-      this.mouse.x = event.pageX;
-      this.mouse.y = event.pageY;
+  mouseMove(event) {
+    this.mouseX = event.pageX;
+    this.mouseY = event.pageY;
+    const instance = this;
 
-      cancelAnimationFrame(this.frameRequest);
+    cancelAnimationFrame(this.frameRequest);
 
-      this.frameRequest = requestAnimationFrame(this.update.bind(this));
-    },
-    setScreenCenter: function () {
-      this.screenCenter.x = window.innerWidth / 2;
-      this.screenCenter.y = window.innerHeight / 2;
-    },
-    update: function () {
-      const dx = this.mouse.x - this.screenCenter.x;
-      const dy = this.mouse.y - this.screenCenter.y;
+    this.frameRequest = requestAnimationFrame(function () {
+      instance.update();
+    });
+  }
 
-      const tiltx = -dy / this.screenCenter.y;
-      const tilty = dx / this.screenCenter.x;
-      const radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2));
-      const degree = radius * 20;
+  setScreenCenter() {
+    this.screenCenter.x = window.innerWidth / 2;
+    this.screenCenter.y = window.innerHeight / 2;
+  }
 
-      gsap.to(elRef.value, {
-        transform: `rotate3d(${tiltx}, ${tilty}, 0, ${degree}deg) perspective(1000px)`,
-        ease: 'power2.out'
-      });
-    },
-    addEventListeners: function () {
-      window.addEventListener('mousemove', this.mouseMove);
-      window.addEventListener('resize', this.setScreenCenter);
-    },
-    removeEventListeners: function () {
-      window.removeEventListener('mousemove', this.mouseMove);
-      window.removeEventListener('resize', this.setScreenCenter);
-    },
-    init: function () {
-      this.mouseMove = this.mouseMove.bind(this);
-      this.setScreenCenter = this.setScreenCenter.bind(this);
+  update() {
+    const dx = this.mouseX - this.screenCenter.x;
+    const dy = this.mouseY - this.screenCenter.y;
 
-      this.setScreenCenter();
+    const tiltx = -dy / this.screenCenter.y;
+    const tilty = dx / this.screenCenter.x;
+    const radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2));
+    const degree = radius * 20;
 
-      this.addEventListeners.bind(this);
-      onUnmounted(this.removeEventListeners.bind(this));
-    }
-  };
-};
+    this.gsap.to(this.elRef.value, {
+      transform: `rotate3d(${tiltx}, ${tilty}, 0, ${degree}deg) perspective(1000px)`,
+      ease: 'power2.out'
+    });
+  }
+
+  addEventListeners() {
+    const instance = this;
+    this.container.addEventListener('mousemove', function (event) {
+      instance.mouseMove(event);
+    });
+    this.container.addEventListener('resize', this.setScreenCenter);
+  }
+
+  removeEventListeners() {
+    const instance = this;
+    this.container.removeEventListener('mousemove', function (event) {
+      instance.mouseMove(event);
+    });
+    this.container.removeEventListener('resize', this.setScreenCenter);
+  }
+}
