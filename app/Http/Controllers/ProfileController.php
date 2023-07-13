7f06fc459e3ee2a2cd46acb0f7158e9da8cd173c
Mike Buonomo <mrb2590@gmail.com>
2023-07-13 23:41:05 +0000

    72-use-sanctum-for-web

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -51,7 +51,7 @@ class ProfileController extends Controller
 
         $user = $request->user();
 
-        Auth::logout();
+        Auth::guard('web')->logout();
 
         $user->delete();
 
--- routes/web.php
+++ routes/web.php
@@ -45,10 +45,10 @@ Route::middleware(['party.verify'])
     });
 
 Route::get('/dashboard', [DashboardController::class, 'show'])
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth:sanctum', 'verified'])
     ->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
