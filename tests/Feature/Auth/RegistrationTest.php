787fb4f049df03bdab6a89386b1e12acfaeba2b9
Mike Buonomo <mrb2590@gmail.com>
2023-07-12 20:16:57 -0400

    Skip tests

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -12,22 +12,24 @@ class RegistrationTest extends TestCase
 
     public function test_registration_screen_can_be_rendered(): void
     {
-        $response = $this->get('/register');
+        static::markTestSkipped('If registration is re-enabled, enable this test.');
+        // $response = $this->get('/register');
 
-        $response->assertStatus(200);
+        // $response->assertStatus(200);
     }
 
     public function test_new_users_can_register(): void
     {
-        $response = $this->post('/register', [
-            'first_name' => 'Test',
-            'last_name' => 'User',
-            'email' => 'test@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password'
-        ]);
+        static::markTestSkipped('If registration is re-enabled, enable this test.');
+        // $response = $this->post('/register', [
+        //     'first_name' => 'Test',
+        //     'last_name' => 'User',
+        //     'email' => 'test@example.com',
+        //     'password' => 'password',
+        //     'password_confirmation' => 'password'
+        // ]);
 
-        $this->assertAuthenticated();
-        $response->assertRedirect(RouteServiceProvider::HOME);
+        // $this->assertAuthenticated();
+        // $response->assertRedirect(RouteServiceProvider::HOME);
     }
 }
